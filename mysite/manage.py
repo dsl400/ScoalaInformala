0043f203b9f2927709d245c07ac3662d21a48f3c
Valentin Sasek <dsl400@gmail.com>
2022-03-03 21:56:10 +0200

    adaugat fixtures

--- mysite/manage.py
+++ mysite/manage.py
@@ -3,6 +3,14 @@
 import os
 import sys
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+import django
+django.setup()
+
+from django.db import connection
+
+
 
 def main():
     """Run administrative tasks."""
@@ -15,6 +23,22 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    if sys.argv[1] == 'runserver':
+        cursor = connection.cursor()
+        tables = connection.introspection.get_table_list(cursor)
+        if len(tables) == 0:
+            execute_from_command_line(['manage.py','migrate'])
+            execute_from_command_line(['manage.py','loaddata','users/fixtures/users.json'])
+            execute_from_command_line(['manage.py','loaddata','posts/fixtures/posts.json'])
+            # execute_from_command_line(['manage.py','loaddata','posts/fixtures/votes.json'])
+        print('##################################################')
+        print('admin: admin@locdedatcucapul.ro')
+        print('pass: test12')
+        print('##################################################')
+        print('user: dsl400@gmail.com')
+        print('pass: RandRand12')
+        print('##################################################')
+
     execute_from_command_line(sys.argv)
 
 
